378732410df36d50291eeef397f49a89e9e30e9e
崔耀 <cuiy@ipharmacare.net>
2024-09-18 17:59:20 +0800

    feat: home 页部分代码提交

--- src/components/Box/index.vue
+++ src/components/Box/index.vue
@@ -1,15 +1,108 @@
 <template>
   <div class="box">
-    
+    <div class="header">
+      <div class="title">
+        {{ title }}
+      </div>
+      <div class="more" @click="toLink">更多>></div>
+    </div>
+    <ul class="contents">
+      <li class="contents-item" v-for="(item,index) in list" :key="index">
+        <span class="contents-item-text">{{item.content}}</span>
+        <span>{{item.date}}</span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data () {
+    return {};
+  },
+  props: {
+    title: {
+      type: String,
+      default: ""
+    },
+    link: {
+      type: String,
+      default: ""
+    },
+    list: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    toLink () {
+      this.$route.push(`${this.link}`);
+    },
+  },
+};
 </script>
 
-<style lang="less" scoped>
-
+<style lang="scss" scoped>
+.box {
+  display: inline-block;
+  height: 319px;
+  background: #fff;
+  .header {
+    height: 54px;
+    display: flex;
+    justify-content: space-between;
+    padding: 0 6px;
+    align-items: center;
+    .title {
+      font-size: 16px;
+      color: #005976;
+      font-weight: bold;
+      display: inline-block;
+      line-height: 54px;
+    }
+    .more {
+      color: #999999;
+      display: inline-block;
+      line-height: 54px;
+      font-size: 14px;
+      font-weight: 400;
+    }
+  }
+  .contents {
+    display: flex;
+    flex-direction: column;
+    padding: 0;
+    margin: 0;
+    min-width: 300px;
+    &-item {
+      display: flex;
+      border-top: 2px dashed #eeeeee;
+      height: 38px;
+      line-height: 38px;
+      position: relative;
+      padding-right: 15px;
+      padding-left: 6px;
+      &-text {
+        text-overflow: ellipsis;
+        overflow: hidden;
+        display: inline-block;
+        white-space: nowrap;
+        width: 200px;
+        padding-left: 15px;
+        text-align: left;
+      }
+      &::before {
+        content: "";
+        width: 4px;
+        height: 4px;
+        display: inline-block;
+        background: #000;
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        left: 6px;
+      }
+    }
+  }
+}
 </style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -8,24 +8,67 @@
       </el-carousel-item>
     </el-carousel>
     <main class="content">
-      模块待开发
+      <el-row>
+        <el-col span="8">
+          <Box title='医院新闻' :list="docNewsList" url="/home" />
+        </el-col>
+        <el-col span="8">
+          <Box title='医院新闻' :list="docNewsList" url="/home" />
+        </el-col>
+        <el-col span="8">
+          <Box title='通知公告' :list="docNewsList" url="/home" />
+        </el-col>
+        <el-col span="8">
+          <Box title='护理园地' :list="docNewsList" url="/home" />
+        </el-col>
+        <el-col span="8">
+          <Box title='医保政策' :list="docNewsList" url="/home" />
+        </el-col>
+        <el-col span="8">
+          <Box title='医院新闻' :list="docNewsList" url="/home" />
+        </el-col>
+      </el-row>
     </main>
   </div>
 </template>
 
 <script>
-
+import Box from "@/components/Box";
 export default {
-  data() {
+  data () {
     return {
       height: '360px',
       imgs: [
         require('../../assets/image/img_banner1.jpg'),
         require('../../assets/image/img_banner2.jpg')
+      ],
+      docNewsList: [
+        {
+          content: '2024年国家网络安全宣传周：网络安全为人民，网络安全靠人民',
+          date: '09-02'
+        }, {
+          content: '工休座谈会：提升服务质量，促进医患沟通',
+          date: '08-23'
+        }, {
+          content: '开心暑假，医起成长” 我院第二届职工子女暑期夏令营活动圆满成功',
+          date: '08-13'
+        }, {
+          content: '医院工会召开2024年高考家长座谈会',
+          date: '08-12'
+        }, {
+          content: '医师节动态 | 科主任查房培训提升临床规范，锤炼诊疗水平',
+          date: '08-09'
+        }, {
+          content: '关注普遍的眼健康 | 南医大四附院开展“全国爱眼日”义诊活动',
+          date: '08-09'
+        },
       ]
     }
   },
-  mounted() {
+  components: {
+    Box
+  },
+  mounted () {
     window.addEventListener('resize', this.resize)
     this.$nextTick(() => {
       setTimeout(() => {
@@ -33,11 +76,11 @@ export default {
       }, 350)
     })
   },
-  beforeDestroy() {
+  beforeDestroy () {
     window.removeEventListener('resize', this.resize)
   },
   methods: {
-    resize() {
+    resize () {
       this.height = this.$refs.imgItems[0].offsetHeight + 'px'
     }
   }
@@ -45,39 +88,52 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.home{
-  .items{
+.home {
+  .items {
     background-size: 100% 100%;
     background-repeat: no-repeat;
   }
-  img{
+  img {
     width: 100%;
     height: auto;
     max-width: 100%;
     max-height: 100%;
     object-fit: cover;
   }
-  .content{
-    width: 100%;
-    height: 300px;
-    line-height: 300px;
-    background: #ccc;
-    text-align: center;
+  .content {
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
   }
-  ::v-deep{
-    .el-carousel__arrow{
+  ::v-deep {
+    .el-carousel__arrow {
       border-radius: 0;
       width: 42px;
       height: 90px;
     }
   }
-  @media screen and (max-width: 768px){
-    ::v-deep{
-    .el-carousel__arrow{
-      width: 32px;
-      height: 42px;
+  @media screen and (max-width: 768px) {
+    ::v-deep {
+      .el-carousel__arrow {
+        width: 32px;
+        height: 42px;
+      }
     }
   }
-  }
+}
+@media screen and (min-width: 1200px) {
+    .content{
+        width: 1180px;
+    }
+}
+@media screen and (min-width: 992px) {
+    .content {
+        width: 970px;
+    }
+}
+@media screen and (min-width: 768px) {
+    .content {
+        width: 750px;
+    }
 }
 </style>
